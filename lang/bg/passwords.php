8b6962ef4b06b22167fc15b0d5e514ead80ed54b
Ilko Ivanov <ilko.iv@gmail.com>
2024-11-26 08:17:26 +0000

    localization

---
+++ lang/bg/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Вашата парола е нулирана.',
+    'sent' => 'Изпратихме по имейл вашата връзка за повторно задаване на парола.',
+    'throttled' => 'Моля, изчакайте, преди да опитате отново.',
+    'token' => 'Това означение за повторно задаване на парола е невалидно.',
+    'user' => "Не можем да намерим потребител с този имейл адрес.",
+
+];
