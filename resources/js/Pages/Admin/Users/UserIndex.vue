bd9da272f9b5e15b8c5cdd6ad3ca04bec1a2ecc4
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-01 13:57:54 +0000

    users table

--- resources/js/Pages/Admin/Users/UserIndex.vue
+++ resources/js/Pages/Admin/Users/UserIndex.vue
@@ -2,125 +2,25 @@
 import AdminLayout from '@/Layouts/AdminLayout.vue'
 import { Head } from '@inertiajs/vue3'
 
+defineProps(['users'])
+
 const columns = [
     {
-        name: 'name',
+        name: 'id',
         required: true,
-        label: 'Dessert (100g serving)',
+        label: '№',
         align: 'left',
-        field: row => row.name,
+        field: row => row.id,
         format: val => `${val}`,
         sortable: true
     },
-    { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
-    { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-    { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-    { name: 'protein', label: 'Protein (g)', field: 'protein' },
-    { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-    { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-    { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
-]
-
-const rows = [
+    { name: 'name', align: 'center', label: 'Име', field: 'name', sortable: true },
+    { name: 'email', label: 'email', field: 'email', sortable: true },
     {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        sodium: 87,
-        calcium: '14%',
-        iron: '1%'
-    },
-    {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        sodium: 129,
-        calcium: '8%',
-        iron: '1%'
-    },
-    {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        sodium: 337,
-        calcium: '6%',
-        iron: '7%'
-    },
-    {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        sodium: 413,
-        calcium: '3%',
-        iron: '8%'
-    },
-    {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        sodium: 327,
-        calcium: '7%',
-        iron: '16%'
-    },
-    {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        sodium: 50,
-        calcium: '0%',
-        iron: '0%'
-    },
-    {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        sodium: 38,
-        calcium: '0%',
-        iron: '2%'
-    },
-    {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        sodium: 562,
-        calcium: '0%',
-        iron: '45%'
-    },
-    {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9,
-        sodium: 326,
-        calcium: '2%',
-        iron: '22%'
-    },
-    {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7,
-        sodium: 54,
-        calcium: '12%',
-        iron: '6%'
+        name: "actions",
+        label: "Управление",
+        align: "center",
+        field: "actions",
     }
 ]
 </script>
@@ -136,12 +36,33 @@ const rows = [
                 flat
                 bordered
                 title="Потребители"
+                rows-per-page="10"
                 rows-per-page-label="Записи на страница"
-                rows-per-page-options=""
-                :rows="rows"
+                :rows="users"
                 :columns="columns"
-                row-key="name"
-            />
+                row-key="id"
+            >
+                <template v-slot:body-cell-actions="props">
+                    <q-td align="center">
+                        <q-btn
+                            icon="edit"
+                            color="primary"
+                            dense
+                            flat
+                            rounded
+                            @click="console.log(props.row.id)"
+                        />
+                        <q-btn
+                            icon="delete"
+                            color="negative"
+                            dense
+                            flat
+                            rounded
+                            @click="console.log(props.row.id)"
+                        />
+                    </q-td>
+                </template>
+            </q-table>
         </q-page>
     </AdminLayout>
 </template>
