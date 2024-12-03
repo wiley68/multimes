05b4d4dec3270d6c4c492acc2179dc2d8256cf85
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-03 05:08:55 +0000

    GuestLayout

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head, router } from '@inertiajs/vue3'
 import { usePermission } from '@/composables/permissions';
+import GuestLayout from '@/Layouts/GuestLayout.vue'
 
 defineProps({
   version: {
@@ -15,44 +16,42 @@ const { hasUser, hasRole } = usePermission()
 <template>
 
   <Head title="Начало"></Head>
-  <q-layout view="hHh lpR fFf">
 
-    <q-page-container>
-      <div
-        class="column absolute-full flex flex-center"
-        style="user-select: none;"
-      >
-        <template v-if="hasUser()">
-          <q-btn
-            v-if="hasRole('admin')"
-            @click="router.get(route('admin.index'))"
-            class="text-primary"
-            icon="dashboard"
-            size="lg"
-            label="Табло"
-          />
-          <q-btn
-            v-else
-            @click="router.get(route('dashboard'))"
-            class="text-primary"
-            icon="dashboard"
-            size="lg"
-            label="Табло"
-          />
-        </template>
-        <template v-else>
-          <q-btn
-            @click="router.get(route('login'))"
-            class="text-primary"
-            icon="login"
-            size="lg"
-            label="Вход"
-          />
-        </template>
-        <h1 class="text-weight-medium text-primary">МУЛТИМЕС</h1>
-        <p class="text-subtitle1 text-weight-light">Laravel v.{{ version }} - Avalon</p>
-      </div>
-    </q-page-container>
+  <GuestLayout>
+    <div
+      class="column absolute-full flex flex-center"
+      style="user-select: none;"
+    >
+      <template v-if="hasUser()">
+        <q-btn
+          v-if="hasRole('admin')"
+          @click="router.get(route('admin.index'))"
+          class="text-primary"
+          icon="dashboard"
+          size="lg"
+          label="Табло"
+        />
+        <q-btn
+          v-else
+          @click="router.get(route('dashboard'))"
+          class="text-primary"
+          icon="dashboard"
+          size="lg"
+          label="Табло"
+        />
+      </template>
+      <template v-else>
+        <q-btn
+          @click="router.get(route('login'))"
+          class="text-primary"
+          icon="login"
+          size="lg"
+          label="Вход"
+        />
+      </template>
+      <h1 class="text-weight-medium text-primary">МУЛТИМЕС</h1>
+      <p class="text-subtitle1 text-weight-light">Laravel v.{{ version }} - Avalon</p>
+    </div>
+  </GuestLayout>
 
-  </q-layout>
 </template>
