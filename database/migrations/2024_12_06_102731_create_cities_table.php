2cb216395044fb9f919f3bed7929fa9e865efa6f
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-06 15:28:57 +0000

    migrate fresh

--- database/migrations/2024_12_06_102731_create_cities_table.php
+++ database/migrations/2024_12_06_102731_create_cities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->text('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -29,7 +29,7 @@ class AdminSeeder extends Seeder
         ])->assignRole('admin');
         User::create([
             'name' => 'Модератор Червенков',
-            'email' => 'ilko.iv@avalonbg.com',
+            'email' => 'ilko.iv@gmail.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
