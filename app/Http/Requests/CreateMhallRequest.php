e1ac279c535deee67f4d3e3bb34564f439b41f2f
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-08 10:06:23 +0000

    create mhall request

--- app/Http/Requests/CreateMhallRequest.php
+++ app/Http/Requests/CreateMhallRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CreateMhallRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class CreateMhallRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,16 @@ class CreateMhallRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('mhalls', 'name')->ignore($this->mhall)
+            ],
+            'factory' => [
+                'required',
+                'array'
+            ]
         ];
     }
 }
