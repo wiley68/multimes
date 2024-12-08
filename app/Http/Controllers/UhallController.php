0825703e9a5ffab35c82065018b200cceb8bbbe9
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-08 12:48:29 +0000

    uhall resource

--- app/Http/Controllers/UhallController.php
+++ app/Http/Controllers/UhallController.php
@@ -4,15 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Uhall;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Response;
 
 class UhallController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request): Response
     {
-        //
+        Gate::authorize('viewAny', Uhall::class);
+        $rowsPerPage = $request->input('rowsPerPage', 10);
+        $page = $request->input('page', 1);
+        $sortBy = $request->input('sortBy', 'id') === null ? 'id' : $request->input('sortBy', 'id');
+        $sortOrder = $request->input('sortOrder', 'asc');
+        $filter = $request->input('filter', '');
+
+        $query = Uhall::query()->with('factory');
+        if (!empty($filter)) {
+            $query->where('name', 'like', '%' . $filter . '%');
+        }
+
+        $mhalls = MhallResource::collection($query->orderBy($sortBy, $sortOrder)
+            ->paginate($rowsPerPage, ['*'], 'page', $page));
+
+        return Inertia::render('Mothers/Mhalls/MhallIndex', [
+            'mhalls' => $mhalls,
+            'filter' => $filter,
+        ]);
     }
 
     /**
