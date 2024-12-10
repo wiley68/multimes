ff7522ccc888005bfb96ad4076aeeada18c06737
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-10 10:05:06 +0000

    dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,12 +8,15 @@ import { Head } from '@inertiajs/vue3'
   <Head title="Табло"></Head>
 
   <DefaultLayout>
-    <q-page class="flex flex-center">
-      <q-card class="q-pa-md">
-        <q-card-section>
-          <div class="text-h6">Табло</div>
-        </q-card-section>
-      </q-card>
-    </q-page>
+    <div
+      class="column"
+      style="height: calc(100vh - 82px); gap: 5px; padding: 5px;"
+    >
+      <div style="flex: 1;display: flex;gap: 5px;">
+        <div style="background-color: brown;flex: 1;">майки</div>
+        <div style="background-color: cadetblue;flex: 1;">угояване</div>
+      </div>
+      <div style="background-color: aquamarine; flex: 1;display: flex;">активни</div>
+    </div>
   </DefaultLayout>
 </template>
