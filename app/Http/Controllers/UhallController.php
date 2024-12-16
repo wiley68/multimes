47e63da2a0814164f2d4254bbb23740f9bb1a759
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-16 14:42:50 +0000

    uproduction show

--- app/Http/Controllers/UhallController.php
+++ app/Http/Controllers/UhallController.php
@@ -68,7 +68,7 @@ class UhallController extends Controller
             'filter' => 'nullable|string|max:255',
         ]);
 
-        $rowsPerPage = $validated['rowsPerPage'] ?? 10;
+        $rowsPerPage = $validated['rowsPerPage'] ?? 6;
         $page = $validated['page'] ?? 1;
         $sortBy = $validated['sortBy'] ?? 'id';
         $sortOrder = $validated['sortOrder'] ?? 'asc';
--- resources/js/Pages/Uproductions/Uhalls/Show.vue
+++ resources/js/Pages/Uproductions/Uhalls/Show.vue
@@ -70,16 +70,6 @@ const onRequest = (requestProp) => {
     );
 }
 
-const activateNavigation = () => {
-    navigationActive.value = true
-}
-
-const deactivateNavigation = () => {
-    navigationActive.value = false
-}
-
-const tableClass = computed(() => navigationActive.value === true ? 'shadow-8 no-outline' : null)
-
 const checkStatus = (val) => {
     if (Array.isArray(val) && val.length > 0) {
         const foundUproject = val.find(item => item.status === 1)
@@ -123,18 +113,103 @@ const confirm = (uhall) => {
     <Head :title="title"></Head>
 
     <DefaultLayout :title="title">
-        <q-page class="q-pa-md">
-            <div class="row items-center justify-between">
-                <div class="col row items-center">
+        <q-page class="q-pa-none">
+            <div class="page-container">
+                <div class="body-panel">
+                    <div class="scrollable-content">
+                        <q-table
+                            grid
+                            grid-header
+                            title="Халета Угояване"
+                            rows-per-page-label="Записи на страница"
+                            no-data-label="Липсват данни"
+                            no-results-label="Няма съответстващи записи"
+                            loading-label="Данните се зареждат..."
+                            :rows="uhalls.data"
+                            :columns="columns"
+                            row-key="name"
+                            :pagination="pagination"
+                            :rows-per-page-options="[6, 9, 12, 15, 18, 0]"
+                            :filter="filter"
+                            hide-header
+                            @request="onRequest"
+                        >
+                            <template v-slot:top-right>
+                                <q-input
+                                    borderless
+                                    dense
+                                    autofocus
+                                    debounce="300"
+                                    v-model="filter"
+                                    placeholder="Търси..."
+                                >
+                                    <template v-slot:append>
+                                        <q-icon name="mdi-magnify" />
+                                    </template>
+                                </q-input>
+                            </template>
+
+                            <template v-slot:item="props">
+                                <div class="q-pa-md col-xs-12 col-sm-6 col-md-4">
+                                    <q-card
+                                        flat
+                                        bordered
+                                    >
+                                        <q-card-section
+                                            class="text-center text-white"
+                                            :class="checkStatus(props.row.uproductions) ? 'bg-accent' : 'bg-grey'"
+                                        >
+                                            <div class="text-h6">{{ props.row.name }}</div>
+                                            <template v-if="checkStatus(props.row.uproductions)">
+                                                <div class="text-subtitle2">Активен производствен процес: №{{
+                                                    checkStatus(props.row.uproductions)
+                                                    }}
+                                                </div>
+                                            </template>
+                                            <template v-else>
+                                                <div class="text-subtitle2">Няма активен производствен процес</div>
+                                            </template>
+                                        </q-card-section>
+                                        <q-separator />
+                                        <q-card-section class="columns flex-center">
+                                            <div>База: {{ props.row.factory.name }}</div>
+                                            <div>Силоз: {{ props.row.silo.name }}</div>
+                                        </q-card-section>
+                                        <q-separator />
+                                        <q-card-actions align="around">
+                                            <template v-if="checkStatus(props.row.uproductions)">
+                                                <q-btn
+                                                    flat
+                                                    :class="checkStatus(props.row.uproductions) ? 'text-accent' : ''"
+                                                    @click="router.get(route('uproductions.show', { uproduction: checkStatus(props.row.uproductions) }))"
+                                                >Управлявай процеса</q-btn>
+                                            </template>
+                                            <template v-else>
+                                                <q-btn
+                                                    v-if="hasPermission('create')"
+                                                    @click="confirm(props.row)"
+                                                    flat
+                                                >Стартирай процес</q-btn>
+                                            </template>
+                                            <q-btn
+                                                flat
+                                                @click="router.get(route('uproductions.index', { uhall: props.row.id }))"
+                                            >Виж процеси</q-btn>
+                                        </q-card-actions>
+                                    </q-card>
+                                </div>
+                            </template>
+                        </q-table>
+                    </div>
+                </div>
+                <div class="footer-panel">
                     <q-btn
                         color="primary"
                         label="Табло"
                         icon="mdi-menu-left"
                         @click="router.get(route('dashboard'))"
                     />
-                </div>
-                <h5 class="col row justify-center items-center">Халета Угояване</h5>
-                <div class="col row justify-end items-center">
+
                     <q-btn
                         label="Всички процеси"
                         icon="mdi-timer-play-outline"
@@ -143,95 +218,6 @@ const confirm = (uhall) => {
                     />
                 </div>
             </div>
-
-            <q-table
-                grid
-                grid-header
-                flat
-                bordered
-                title="Халета Угояване"
-                rows-per-page-label="Записи на страница"
-                no-data-label="Липсват данни"
-                no-results-label="Няма съответстващи записи"
-                loading-label="Данните се зареждат..."
-                :class="tableClass"
-                :rows="uhalls.data"
-                :columns="columns"
-                row-key="name"
-                :pagination="pagination"
-                :rows-per-page-options="[6, 9, 12, 15, 18, 0]"
-                :filter="filter"
-                hide-header
-                @request="onRequest"
-                @focusin="activateNavigation"
-                @focusout="deactivateNavigation"
-            >
-                <template v-slot:top-right>
-                    <q-input
-                        borderless
-                        dense
-                        autofocus
-                        debounce="300"
-                        v-model="filter"
-                        placeholder="Търси..."
-                    >
-                        <template v-slot:append>
-                            <q-icon name="mdi-magnify" />
-                        </template>
-                    </q-input>
-                </template>
-
-                <template v-slot:item="props">
-                    <div class="q-pa-md col-xs-12 col-sm-6 col-md-4">
-                        <q-card
-                            flat
-                            bordered
-                        >
-                            <q-card-section
-                                class="text-center text-white"
-                                :class="checkStatus(props.row.uproductions) ? 'bg-accent' : 'bg-grey'"
-                            >
-                                <div class="text-h6">{{ props.row.name }}</div>
-                                <template v-if="checkStatus(props.row.uproductions)">
-                                    <div class="text-subtitle2">Активен производствен процес: №{{
-                                        checkStatus(props.row.uproductions)
-                                    }}
-                                    </div>
-                                </template>
-                                <template v-else>
-                                    <div class="text-subtitle2">Няма активен производствен процес</div>
-                                </template>
-                            </q-card-section>
-                            <q-separator />
-                            <q-card-section class="columns flex-center">
-                                <div>База: {{ props.row.factory.name }}</div>
-                                <div>Силоз: {{ props.row.silo.name }}</div>
-                            </q-card-section>
-                            <q-separator />
-                            <q-card-actions align="around">
-                                <template v-if="checkStatus(props.row.uproductions)">
-                                    <q-btn
-                                        flat
-                                        :class="checkStatus(props.row.uproductions) ? 'text-accent' : ''"
-                                        @click="router.get(route('uproductions.show', { uproduction: checkStatus(props.row.uproductions) }))"
-                                    >Управлявай процеса</q-btn>
-                                </template>
-                                <template v-else>
-                                    <q-btn
-                                        v-if="hasPermission('create')"
-                                        @click="confirm(props.row)"
-                                        flat
-                                    >Стартирай процес</q-btn>
-                                </template>
-                                <q-btn
-                                    flat
-                                    @click="router.get(route('uproductions.index', { uhall: props.row.id }))"
-                                >Виж процеси</q-btn>
-                            </q-card-actions>
-                        </q-card>
-                    </div>
-                </template>
-            </q-table>
         </q-page>
     </DefaultLayout>
 </template>
