8a9c40a2be750b2fed1c8b00fd8f677ad2b624a5
Ilko Ivanov <ilko.iv@gmail.com>
2024-12-16 15:11:13 +0000

    silo

--- resources/js/Pages/Nomenklature/Silos/Edit.vue
+++ resources/js/Pages/Nomenklature/Silos/Edit.vue
@@ -35,9 +35,57 @@ const title = 'Промяна на Силоз'
     <Head :title="title"></Head>
 
     <DefaultLayout :title="title">
-        <q-page class="q-pa-md column">
-            <div class="row items-center justify-between">
-                <div class="col row items-center">
+        <q-page class="q-pa-none">
+            <div class="page-container">
+                <div class="body-panel">
+                    <div class="scrollable-content">
+                        <div class="column flex-grow flex-center">
+                            <q-card
+                                class="q-pa-md"
+                                style="width: 800px; max-width: 100%;"
+                            >
+                                <q-form
+                                    @submit.prevent="onSubmit"
+                                    @reset="onReset"
+                                    class="q-gutter-md"
+                                >
+                                    <q-input
+                                        v-model="form.name"
+                                        label="Силоз *"
+                                        hint="Име на Силоза"
+                                        :error="form.hasErrors"
+                                        :error-message="form.errors.name"
+                                    />
+
+                                    <q-select
+                                        v-model="form.factory"
+                                        :options="factories"
+                                        option-label="name"
+                                        label="Избери база"
+                                        :error="form.hasErrors"
+                                        :error-message="form.errors.factory"
+                                    />
+
+                                    <div>
+                                        <q-btn
+                                            label="Промени"
+                                            type="submit"
+                                            color="primary"
+                                        />
+                                        <q-btn
+                                            label="Откажи"
+                                            type="reset"
+                                            color="primary"
+                                            flat
+                                            class="q-ml-sm"
+                                        />
+                                    </div>
+                                </q-form>
+                            </q-card>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-panel">
                     <q-btn
                         color="primary"
                         label="Силози"
@@ -45,53 +93,6 @@ const title = 'Промяна на Силоз'
                         @click="router.get(route('silos.index'))"
                     />
                 </div>
-                <h5 class="col row justify-center items-center">Промяна на Силоз</h5>
-                <div class="col row justify-end items-center"></div>
-            </div>
-            <div class="column flex-grow flex-center">
-                <q-card
-                    class="q-pa-md"
-                    style="width: 800px; max-width: 100%;"
-                >
-                    <q-form
-                        @submit.prevent="onSubmit"
-                        @reset="onReset"
-                        class="q-gutter-md"
-                    >
-                        <q-input
-                            v-model="form.name"
-                            label="Силоз *"
-                            hint="Име на Силоза"
-                            :error="form.hasErrors"
-                            :error-message="form.errors.name"
-                        />
-
-                        <q-select
-                            v-model="form.factory"
-                            :options="factories"
-                            option-label="name"
-                            label="Избери база"
-                            :error="form.hasErrors"
-                            :error-message="form.errors.factory"
-                        />
-
-                        <div>
-                            <q-btn
-                                label="Промени"
-                                type="submit"
-                                color="primary"
-                            />
-                            <q-btn
-                                label="Откажи"
-                                type="reset"
-                                color="primary"
-                                flat
-                                class="q-ml-sm"
-                            />
-                        </div>
-                    </q-form>
-                </q-card>
-
             </div>
         </q-page>
     </DefaultLayout>
