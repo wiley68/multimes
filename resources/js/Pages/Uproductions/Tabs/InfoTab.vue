d62374a8e98ddf5b812395a84e5fe0070217cd89
Ilko Ivanov <ilko.iv@gmail.com>
2025-01-05 08:29:27 +0000

    info tab

--- resources/js/Pages/Uproductions/Tabs/InfoTab.vue
+++ resources/js/Pages/Uproductions/Tabs/InfoTab.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { computed } from 'vue';
+import { computed } from 'vue'
+import moment from 'moment'
 
 
 const props = defineProps({
@@ -9,10 +10,21 @@ const props = defineProps({
     },
 })
 
+const getDaysBetweenTodayAndDate = (targetDate) => {
+    const today = moment().startOf('day')
+    const target = moment(targetDate).startOf('day')
+    return today.diff(target, 'days')
+}
+
 const siloPurcent = computed(() => {
     return parseFloat((parseFloat(props.uproduction.uhall.silo.stock) / parseFloat(props.uproduction.uhall.silo.maxqt)).toFixed(2))
 })
 const siloPurcentLabel = `${(siloPurcent.value * 100).toFixed(2)}%`
+const productionPurcent = computed(() => {
+    const days = getDaysBetweenTodayAndDate(props.uproduction.created_at)
+    return parseFloat((days / parseFloat(props.uproduction.production_days)).toFixed(2))
+})
+const productionPurcentLabel = `${(productionPurcent.value * 100).toFixed(2)}%`
 </script>
 
 <template>
@@ -95,7 +107,42 @@ const siloPurcentLabel = `${(siloPurcent.value * 100).toFixed(2)}%`
             </q-card-section>
             <q-separator />
             <q-card-section class="col">
-
+                <div class="text-subtitle1"><span class="text-weight-medium">Производствен процес №</span><span
+                        class="text-weight-light"
+                    >: {{ uproduction.id }}</span>
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Стартиран на</span><span
+                        class="text-weight-light"
+                    >: {{ moment(uproduction.created_at).format('DD.MM.YY HH:mm') }}</span>
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Състояние</span><span
+                        class="text-weight-light"
+                    >: {{ uproduction.status === 1 ? 'Активен' : 'Приключен' }}</span>
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Очакван брой дни за провеждане на
+                        процеса</span><span class="text-weight-light">: {{ uproduction.production_days }}</span>
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Продукт</span><span
+                        class="text-weight-light"
+                    >: {{ `[${uproduction.product.nomenklature}] ${uproduction.product.name}` }}</span>
+                </div>
+                <div class="text-subtitle2 text-weight-light">{{ uproduction.product.description }}
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Текущ брой прасета [бр]</span><span
+                        class="text-weight-light"
+                    >: {{ uproduction.stock }}</span>
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Брой дни в процес</span><span
+                        class="text-weight-light"
+                    >: {{ getDaysBetweenTodayAndDate(uproduction.created_at) }}</span>
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Оставащи дни до края на процеса</span><span
+                        class="text-weight-light"
+                    >: {{ uproduction.production_days - getDaysBetweenTodayAndDate(uproduction.created_at) }}</span>
+                </div>
+                <div class="text-subtitle1"><span class="text-weight-medium">Процент на завършеност на
+                        процеса</span><span class="text-weight-light">: {{ productionPurcentLabel }}</span>
+                </div>
             </q-card-section>
             <q-card-actions vertical>
 
