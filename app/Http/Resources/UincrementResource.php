d4e47f07062c5b123bc7d720e52bafcbe20dae98
Ilko Ivanov <ilko.iv@gmail.com>
2025-01-06 06:59:09 +0000

    UincrementResource

---
+++ app/Http/Resources/UincrementResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UincrementResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'uproduction' => new UproductionsResource($this->whenLoaded('uproduction')),
+            'product' => new ProductResource($this->whenLoaded('product')),
+            'quantity' => $this->quantity,
+            'price' => $this->price,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
