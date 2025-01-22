826c8342df8e85993716c27d32b7efa8348dbef1
Ilko Ivanov <ilko.iv@gmail.com>
2025-01-22 16:43:29 +0000

    product index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -31,7 +31,7 @@ class ProductController extends Controller
         $rowsPerPage = $validated['rowsPerPage'] ?? 10;
         $page = $validated['page'] ?? 1;
         $sortBy = $validated['sortBy'] ?? 'id';
-        $sortOrder = $validated['sortOrder'] ?? 'asc';
+        $sortOrder = $validated['sortOrder'] ?? 'desc';
         $filter = $validated['filter'] ?? '';
 
         $query = Product::query();
