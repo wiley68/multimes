329193a6e8eb4fbdcfc86ee27228ff8cf451bdc2
Ilko Ivanov <ilko.iv@gmail.com>
2025-01-25 16:33:11 +0000

    mdecrementresource mincrementresource

---
+++ app/Http/Resources/MdecrementResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MdecrementResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'mproduction' => new MproductionsResource($this->whenLoaded('mproduction')),
+            'product' => new ProductResource($this->whenLoaded('product')),
+            'quantity' => $this->quantity,
+            'price' => $this->price,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
