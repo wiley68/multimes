1fa15dd6174851c06dc33a1778442ebfa3c55217
Ilko Ivanov <ilko.iv@gmail.com>
2025-02-27 09:44:23 +0000

    load uproduction

--- app/Http/Requests/LoadUproductionRequest.php
+++ app/Http/Requests/LoadUproductionRequest.php
@@ -37,16 +37,6 @@ class LoadUproductionRequest extends FormRequest
                 'numeric',
                 'min:0',
             ],
-            'group_number' => [
-                'required',
-                'integer',
-                'min:0',
-            ],
-            'partida_number' => [
-                'required',
-                'integer',
-                'min:0',
-            ],
             'product' => [
                 'required',
                 'array',
